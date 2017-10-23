43628905deabf28f9813ac30aa84243fa207fbff
Bram <bramerto@users.noreply.github.com>
2017-10-23 09:42:18 +0200

    Gamesetup
    Eerste gamesetup voor CB

---
+++ OOPG-ArcadaShooter/src/main/java/ArcadaShooter/ArcadaShooter.java
@@ -0,0 +1,24 @@
+package ArcadaShooter;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+
+public class ArcadaShooter extends GameEngine {
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OOPG-ArcadaShooter/src/main/java/ArcadaShooter/Enemy.java
@@ -0,0 +1,46 @@
+package ArcadaShooter;
+
+import java.util.List;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.CollidedTile;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithTiles;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import processing.core.PGraphics;
+
+public class Enemy extends GameObject implements ICollidableWithGameObjects, ICollidableWithTiles {
+
+	public Enemy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Enemy(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void draw(PGraphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
